13918e7801b30361c2e99ea11a13261dcd97c610
Eric Wang <ericletsgo@gmail.com>
2019-05-03 17:20:48 -0700

    Added Footer component with respective text

--- app/components/Footer/index.js
+++ app/components/Footer/index.js
@@ -3,6 +3,7 @@ import { FormattedMessage } from 'react-intl';
 
 import A from 'components/A';
 import LocaleToggle from 'containers/LocaleToggle';
+// import EmailSignUp from 'components/Footer/EmailSignUp';
 import Wrapper from './Wrapper';
 import messages from './messages';
 
@@ -10,18 +11,38 @@ function Footer() {
   return (
     <Wrapper>
       <section>
-        <FormattedMessage {...messages.licenseMessage} />
-      </section>
-      <section>
-        <LocaleToggle />
+        <FormattedMessage {...messages.addressMessage} />
       </section>
       <section>
         <FormattedMessage
-          {...messages.authorMessage}
+          {...messages.phoneNumberTollFreeMessage}
           values={{
-            author: <A href="https://twitter.com/mxstbr">Max Stoiber</A>,
+            tollFreeNumber: <A href="tel:+1-800-540-5823">1-(800)-540-5823</A>,
           }}
         />
+      </section>
+      <section>
+        <FormattedMessage
+          {...messages.phoneNumberInternationalMessage}
+          values={{
+            internationalNumber: (
+              <A href="tel:1-909-865-3081">1-(909)-865-3081</A>
+            ),
+          }}
+        />
+      </section>
+      <section>
+        <FormattedMessage
+          {...messages.copyRightsMessage}
+          values={{
+            goldenWestLink: (
+              <A href="https://gwlinc.com/">GoldenWest Lubricants, Inc.</A>
+            ),
+          }}
+        />
+      </section>
+      <section>
+        <LocaleToggle />
       </section>
     </Wrapper>
   );
--- app/components/Footer/messages.js
+++ app/components/Footer/messages.js
@@ -8,14 +8,28 @@ import { defineMessages } from 'react-intl';
 export const scope = 'boilerplate.components.Footer';
 
 export default defineMessages({
-  licenseMessage: {
-    id: `${scope}.license.message`,
-    defaultMessage: 'This project is licensed under the MIT license.',
+  addressMessage: {
+    id: `${scope}.address.message`,
+    defaultMessage: '1937 Mount Vernon Avenue, CA 91768',
   },
-  authorMessage: {
-    id: `${scope}.author.message`,
+  phoneNumberTollFreeMessage: {
+    id: `${scope}.phoneNumberTollFree.message`,
     defaultMessage: `
-      Made with love by {author}.
+      Toll Free: {tollFreeNumber} Fax: 1-(800)-966-5823
     `,
+  },
+  phoneNumberInternationalMessage: {
+    id: `${scope}.phoneNumberInternational.message`,
+    defaultMessage: `
+      International: {internationalNumber} Fax: 1-(909)-865-4467
+    `,
+  },
+  hoursMessage: {
+    id: `${scope}.hours.message`,
+    defaultMessage: 'Hours of Operation: 8:00 am - 5:00 pm PDT/PST',
+  },
+  copyRightsMessage: {
+    id: `${scope}.copyRights.message`,
+    defaultMessage: `	© 2019 {goldenWestLink}`,
   },
 });
