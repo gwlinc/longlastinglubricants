965be0e17779caa6eae52ac7cc57b6397929966d
Eric Wang <ericletsgo@gmail.com>
2019-07-30 14:54:16 -0700

    Added cards section component

---
+++ app/components/CardsSection/messages.js
@@ -0,0 +1,16 @@
+/*
+ * CardsSection Messages
+ *
+ * This contains all the text for the CardsSection component.
+ */
+
+import { defineMessages } from 'react-intl';
+
+export const scope = 'app.components.CardsSection';
+
+export default defineMessages({
+  header: {
+    id: `${scope}.header`,
+    defaultMessage: 'This is the CardsSection component!',
+  },
+});
