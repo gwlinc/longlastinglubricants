53337ba107857226535bf34388949a45502a2b1b
Eric Wang <ericletsgo@gmail.com>
2019-08-02 03:20:19 -0700

    Working implementation of about us section

--- app/components/AboutUs/index.js
+++ app/components/AboutUs/index.js
@@ -5,9 +5,15 @@
  */
 
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { fade, makeStyles, withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
+import InputBase from '@material-ui/core/InputBase';
+import Typography from '@material-ui/core/Typography';
+import FormControl from '@material-ui/core/FormControl';
+import Icon from '@material-ui/core/Icon';
+import Button from '@material-ui/core/Button';
+import InputLabel from '@material-ui/core/InputLabel';
 import { FormattedMessage } from 'react-intl';
 import Img from './Img';
 import HQ from './HQ.jpg';
@@ -15,6 +21,41 @@ import HQ from './HQ.jpg';
 // import styled from 'styled-components';
 
 import messages from './messages';
+
+const Input = withStyles(theme => ({
+  root: {
+    'label + &': {
+      marginTop: theme.spacing(3),
+    },
+  },
+  input: {
+    borderRadius: 4,
+    position: 'relative',
+    backgroundColor: theme.palette.common.white,
+    border: '1px solid #ced4da',
+    fontSize: 16,
+    width: 'auto',
+    padding: '10px 12px',
+    transition: theme.transitions.create(['border-color', 'box-shadow']),
+    // Use the system font instead of the default Roboto font.
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+    '&:focus': {
+      boxShadow: `${fade(theme.palette.primary.main, 0.25)} 0 0 0 0.2rem`,
+      borderColor: theme.palette.primary.main,
+    },
+  },
+}))(InputBase);
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -26,8 +67,34 @@ const useStyles = makeStyles(theme => ({
     height: '100%',
     paddingTop: 10,
   },
-  grid: {},
+  messageText: {
+    textAlign: 'justify',
+    justifyContent: 'center',
+    color: 'white',
+    margin: 40,
+    textJustify: 'interword',
+  },
+  signUpMessage: {
+    textAlign: 'center',
+    padding: 12,
+    color: '#1a237e',
+    paddingLeft: 30,
+    fontWeight: 'bold',
+  },
+  input: {
+    padding: 6,
+    paddingLeft: 30
+  },
+  rightIcon: {
+    marginLeft: theme.spacing(1),
+  },
+  button: {
+    margin: theme.spacing(1),
+    marginLeft: 35,
+  },
 }));
+
+
 
 function AboutUs() {
   const classes = useStyles();
@@ -44,7 +111,26 @@ function AboutUs() {
           <Grid item xs={5}>
             <Img src={HQ} />
           </Grid>
-          <Grid item xs={7} />
+          <Grid item xs={7} >
+          <Typography className={classes.messageText} variant="body2" component="p">
+            <FormattedMessage {...messages.message} />
+          </Typography>
+          </Grid>
+        </Grid>
+        <Grid container spacing={0}>
+          <Grid item xs={7} >
+            <Typography  className={classes.signUpMessage} variant="h6" >
+              <FormattedMessage {...messages.signUp} />
+            </Typography>
+          </Grid>
+          <Grid item xs={3}>
+            <FormControl className={classes.input}>
+              <Input defaultValue="YOUR EMAIL" />
+            </FormControl>
+          </Grid>
+          {/* <Button item xs={3} variant="contained" color="primary" className={classes.button}>
+            <Icon className={classes.rightIcon} > </Icon>
+          </Button> */}
         </Grid>
       </Box>
     </div>
--- app/components/AboutUs/messages.js
+++ app/components/AboutUs/messages.js
@@ -9,8 +9,12 @@ import { defineMessages } from 'react-intl';
 export const scope = 'app.components.AboutUs';
 
 export default defineMessages({
-  header: {
-    id: `${scope}.header`,
-    defaultMessage: 'This is the AboutUs component!',
+  message: {
+    id: `${scope.message}`,
+    defaultMessage: `Prolong Super Lubricants, a brand of GoldenWest Lubricants Inc., is the perfect example of an American success story. In the early 1990's, the founders started in the back of a small shop, packing the product themselves. Today, millions of bottles of Prolong products have been sold all over the world.`
   },
+  signUp: {
+    id: `${scope.signUp}`,
+    defaultMessage: 'SIGN UP FOR NEWS & SPECIAL OFFERS',
+  }
 });
